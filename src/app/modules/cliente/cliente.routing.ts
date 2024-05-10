75b77ba615b75cd6008d01c74864da5159bd5680
Gabriel Santos <gabrielosantosb@outlook.com>
2024-05-10 17:06:57 -0300

    Refatorando pastas

---
+++ src/app/modules/cliente/cliente.routing.ts
@@ -0,0 +1,11 @@
+import {Routes} from "@angular/router";
+import {ClienteHomeComponent} from "./page/cliente-home/cliente-home.component";
+
+export const CLIENTE_ROUTES: Routes =
+  [
+    // {
+    //   path : "",
+    //   component: ClienteHomeComponent
+    // }
+
+  ]
