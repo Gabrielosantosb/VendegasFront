e07dc2f8936a9b24c238ee109a5c2e02cb2dcf69
Gabriel Santos <gabrielosantosb@outlook.com>
2024-05-11 14:37:56 -0300

    Adicionando service produto

---
+++ src/app/services/produto/produto.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import {environments} from "../../../environments/environments";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {CookieService} from "ngx-cookie-service";
+import {Observable} from "rxjs";
+import {ProdutoResponse} from "../../../models/interfaces/produto/response/ProdutoResponse";
+import {ProdutoRequest} from "../../../models/interfaces/produto/request/ProdutoRequest";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProdutoService {
+
+  private API_URL = environments.API_URL;
+  private readonly USER_AUTH = environments.COOKIES_VALUE.user_auth
+  private token = this.cookie.get(this.USER_AUTH)
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${this.token}`
+    })
+  }
+  constructor(private http: HttpClient, private cookie: CookieService) {}
+
+  getAllProdutos(): Observable<Array<ProdutoResponse>> {
+    return this.http.get<Array<ProdutoResponse>>(
+      `${this.API_URL}/api/Produto/get-all-produtos`,
+      this.httpOptions
+    )
+  }
+
+  createProduto(empresaId: number, requestData : ProdutoRequest): Observable<Array<ProdutoResponse>> {
+    console.log('Request:', requestData)
+    return this.http.post<Array<ProdutoResponse>>(
+      `${this.API_URL}/api/Produto/create-produto/${empresaId}`,
+      requestData,
+      this.httpOptions
+    );
+  }
+
+
+
+
+}
+
---
+++ src/models/interfaces/produto/response/ProdutoResponse.ts
@@ -0,0 +1,8 @@
+export interface ProdutoResponse
+{
+  produtoId: number,
+  nome: string,
+  valor: string,
+  descricao: string,
+  empresaId: number
+}
